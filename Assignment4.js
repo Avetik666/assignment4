83e5cfa974966dba17e1663f612b545482e956eb
Avetik <avetik.sarikyan@wicastr.com>
2021-08-06 23:46:02 +0400

    Add missing exercises 2,3,4

--- Assignment4.js
+++ Assignment4.js
@@ -4,5 +4,12 @@ let b = 10;
 let cSQR = a*a + b*b;
 
 //Ex.2
+for(let i = 0; i<5; i++){
+    console.log('SOMething');
+}
 //Ex.3
+console.log('Ex 3');
 //Ex.4
+if(a < 10){
+    console.log('true')
+}
